e0713f4b3963205e6be31f2db5b3082eecdf6e52
Jairo Nascimento <jaironsousa@gmail.com>
2024-07-03 11:31:05 -0300

    Utilizando Mockito para os testes de unidade

--- src/test/java/com/jnsdev/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/jnsdev/swplanetapi/domain/PlanetServiceTest.java
@@ -3,27 +3,44 @@ package com.jnsdev.swplanetapi.domain;
 import com.jnsdev.swplanetapi.common.PlanetsConstants;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static com.jnsdev.swplanetapi.common.PlanetsConstants.PLANET;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 /**
  * @Autor Jairo Nascimento
  * @Created 03/07/2024 - 10:48
  */
-@SpringBootTest(classes = PlanetService.class)
+//@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 class PlanetServiceTest {
 
-    @Autowired
+    @InjectMocks
     private PlanetService planetService;
+
+    //    @MockBean
+    @Mock
+    private PlanetRepository planetRepository;
 
     // operacao_estado_returno
     @Test
     void createPlanet_withValiData_ReturnsPlanet() {
+        // Arrange
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+        // ACT
         Planet sut = planetService.create(PLANET);
 
+        // Asserts
         Assertions.assertThat(sut).isEqualTo(PLANET);
 
     }
