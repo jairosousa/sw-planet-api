02aa1d1478e366ae09757a2c2792be3de24d9b35
Jairo Nascimento <jaironsousa@gmail.com>
2024-07-18 10:18:01 -0300

    Testando o cenário de sucesso no repository

---
+++ src/test/java/com/jnsdev/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.jnsdev.swplanetapi.domain;
+
+import com.jnsdev.swplanetapi.common.PlanetsConstants;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static com.jnsdev.swplanetapi.common.PlanetsConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 18/07/2024 - 09:48
+ */
+@DataJpaTest
+class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        System.out.println(sut);
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getId()).isEqualTo(planet.getId());
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+}
