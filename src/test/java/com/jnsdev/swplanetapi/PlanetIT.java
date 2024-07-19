1ea1a0090cefd1c3c22ca0adfd51eaaec2770420
Jairo Nascimento <jaironsousa@gmail.com>
2024-07-19 11:23:54 -0300

    Testando o cadastro de usuário com sucesso

--- src/test/java/com/jnsdev/swplanetapi/PlanetIT.java
+++ src/test/java/com/jnsdev/swplanetapi/PlanetIT.java
@@ -1,10 +1,20 @@
 package com.jnsdev.swplanetapi;
 
+import com.jnsdev.swplanetapi.common.PlanetsConstants;
+import com.jnsdev.swplanetapi.domain.Planet;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import static com.jnsdev.swplanetapi.common.PlanetsConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.CREATED;
 
 /**
  * @Autor Jairo Nascimento
@@ -14,8 +24,17 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class PlanetIT {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
     }
 }
