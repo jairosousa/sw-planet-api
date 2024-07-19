ff627a55f4da9c6dc3185e3bbe9d8acb1a6b7ada
Jairo Nascimento <jaironsousa@gmail.com>
2024-07-19 10:21:06 -0300

    Configuração do servidor de aplicação

---
+++ src/test/java/com/jnsdev/swplanetapi/PlanetIT.java
@@ -0,0 +1,19 @@
+package com.jnsdev.swplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 19/07/2024 - 09:37
+ */
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class PlanetIT {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
