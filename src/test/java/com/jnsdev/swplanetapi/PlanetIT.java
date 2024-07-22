3d17bae997c25191a872dd366c3964645ff527ee
Jairo Nascimento <jaironsousa@gmail.com>
2024-07-22 10:20:32 -0300

    Testando a consulta de planeta com sucesso

--- src/test/java/com/jnsdev/swplanetapi/PlanetIT.java
+++ src/test/java/com/jnsdev/swplanetapi/PlanetIT.java
@@ -1,21 +1,20 @@
 package com.jnsdev.swplanetapi;
 
-import com.jnsdev.swplanetapi.common.PlanetsConstants;
 import com.jnsdev.swplanetapi.domain.Planet;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import static com.jnsdev.swplanetapi.common.PlanetsConstants.PLANET;
+import static com.jnsdev.swplanetapi.common.PlanetsConstants.TATOOINE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * @Autor Jairo Nascimento
@@ -23,12 +22,13 @@ import static org.springframework.http.HttpStatus.CREATED;
  */
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@Sql(scripts = {"/import_planets.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class PlanetIT {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Sql(scripts = {"/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Test
     void createPlanet_ReturnsCreated() {
         ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
@@ -38,5 +38,12 @@ class PlanetIT {
         assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
         assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
         assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    void createPlanet_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
+        assertThat(sut.getStatusCode()).isEqualTo(OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
     }
 }
